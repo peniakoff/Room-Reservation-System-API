7d7c27077b88344b317ffbf8082153372726a286
Tomasz Miller <info@tomaszmiller.pl>
2017-07-06 12:14:15 +0200

    Spring's project's files added

---
+++ src/main/java/pl/tomaszmiller/roomreservationsystemAPI/RoomReservationSystemApiApplication.java
@@ -0,0 +1,12 @@
+package pl.tomaszmiller.roomreservationsystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationSystemApiApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/tomaszmiller/roomreservationsystemAPI/RoomReservationSystemApiApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.tomaszmiller.roomreservationsystemAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoomReservationSystemApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
