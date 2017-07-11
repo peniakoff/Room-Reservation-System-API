d9036e644b893b333a943d85f36ec0926a19b0ec
Tomasz Miller <info@tomaszmiller.pl>
2017-07-11 14:39:51 +0200

    method to find by email added

--- src/main/java/pl/tomaszmiller/roomreservationsystemAPI/UserRepository.java
+++ src/main/java/pl/tomaszmiller/roomreservationsystemAPI/UserRepository.java
@@ -1,7 +1,15 @@
 package pl.tomaszmiller.roomreservationsystemAPI;
 
+import org.springframework.data.repository.CrudRepository;
+import pl.tomaszmiller.roomreservationsystemAPI.models.User;
+
+import java.util.Optional;
+
 /**
  * Created by Tomasz on 11.07.2017.
  */
-public class UserRepository {
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
 }
