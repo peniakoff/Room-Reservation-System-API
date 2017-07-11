83bb54c08afc82f7d7c6b8dba31a454327f117d3
Tomasz Miller <info@tomaszmiller.pl>
2017-07-11 12:20:21 +0200

    ApiController, Room class, User class & Reservation class created

---
+++ src/main/java/pl/tomaszmiller/roomreservationsystemAPI/controllers/ApiController.java
@@ -0,0 +1,7 @@
+package pl.tomaszmiller.roomreservationsystemAPI.controllers;
+
+/**
+ * Created by Tomasz on 11.07.2017.
+ */
+public class ApiController {
+}
---
+++ src/main/java/pl/tomaszmiller/roomreservationsystemAPI/models/User.java
@@ -0,0 +1,7 @@
+package pl.tomaszmiller.roomreservationsystemAPI.models;
+
+/**
+ * Created by Tomasz on 11.07.2017.
+ */
+public class User {
+}
