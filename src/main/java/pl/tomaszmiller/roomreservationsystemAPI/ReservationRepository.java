ce8457bd228724992c880efa6516a9de64c0b009
Tomasz Miller <info@tomaszmiller.pl>
2017-07-11 14:39:29 +0200

    methods to find all added

--- src/main/java/pl/tomaszmiller/roomreservationsystemAPI/ReservationRepository.java
+++ src/main/java/pl/tomaszmiller/roomreservationsystemAPI/ReservationRepository.java
@@ -1,7 +1,15 @@
-package pl.tomaszmiller.roomreservationsystemAPI.models;
+package pl.tomaszmiller.roomreservationsystemAPI;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.tomaszmiller.roomreservationsystemAPI.models.Reservation;
+
+import java.util.List;
 
 /**
  * Created by Tomasz on 11.07.2017.
  */
-public interface ReservationRepository {
+public interface ReservationRepository extends CrudRepository<Reservation, Integer> {
+
+    List<Reservation> findAll();
+
 }
--- src/main/java/pl/tomaszmiller/roomreservationsystemAPI/RoomRepository.java
+++ src/main/java/pl/tomaszmiller/roomreservationsystemAPI/RoomRepository.java
@@ -1,7 +1,15 @@
 package pl.tomaszmiller.roomreservationsystemAPI;
 
+import org.springframework.data.repository.CrudRepository;
+import pl.tomaszmiller.roomreservationsystemAPI.models.Room;
+
+import java.util.List;
+
 /**
  * Created by Tomasz on 11.07.2017.
  */
-public class RoomRepository {
+public interface RoomRepository extends CrudRepository<Room, Integer> {
+
+    List<Room> findAll();
+
 }
